ff397f6642370d6da72cfb713a69653ce126e617
sianblone <sianblone@gmail.com>
2020-05-07 17:57:17 +0900

    Add: New Algorithm

--- JavaProject/src/com/java/test/Main.java
+++ JavaProject/src/com/java/test/Main.java
@@ -3,6 +3,11 @@ package com.java.test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 	
@@ -10,36 +15,48 @@ public class Main {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    
-		String dartResult = br.readLine();
-		char[] arrChar = dartResult.toCharArray();
+		int N = 5;
+		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+		HashMap<Integer, Double> failureMap = new HashMap<>();
 		
-		int[] arrScore = new int[3];
-		int index = -1;
-		int sum = 0;
-		
-		for(int i = 0 ; i < arrChar.length ; i++) {
-			if(arrChar[i] == '1' && arrChar[i+1] == '0') {
-				arrScore[++index] = 10;
-				i++;
-			} else if(arrChar[i] >= '0' && arrChar[i] <= '9') {
-				arrScore[++index] = arrChar[i] - '0';
-			} else if(arrChar[i] == 'D') {
-				arrScore[index] = (int) Math.pow(arrScore[index], 2);
-			} else if(arrChar[i] == 'T') {
-				arrScore[index] = (int) Math.pow(arrScore[index], 3);
-			} else if(arrChar[i] == '*') {
-				arrScore[index] *= 2;
-				if(index > 0) {
-					arrScore[index-1] *= 2;
+		for(int i = 0 ; i < N ; i++) {
+			int countArrival = 0;
+			int countFailure = 0;
+			
+			for(int j = 0 ; j < stages.length ; j++) {
+				// stages 배열[j]의 값이
+				// 현재 실패율 구하는 스테이지 번호 (i+1)과 같으면 n스테이지 도달 수, 실패 수 카운트 ++
+				// (인덱스는 0부터 시작이므로 실제로 1스테이지는 인덱스 0번에 저장된다)
+				if(stages[j] == i+1) {
+					countArrival++;
+					countFailure++;
+				} else if(stages[j] > i+1) {
+					// 현재 실패율 구하는 스테이지 번호보다 크면 도달 수 카운트 ++
+					countArrival++;
 				}
-			} else if(arrChar[i] == '#') {
-				arrScore[index] *= -1;
 			}
+			failureMap.put(i+1, (double)countFailure / (double)countArrival);
 		}
 		
-		for(int i = 0 ; i < arrScore.length ; i++) {
-			sum += arrScore[i];
+		List<Integer> keySetList = new ArrayList<>(failureMap.keySet());
+		
+		Collections.sort(keySetList, new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if(failureMap.get(o1) < failureMap.get(o2)) {
+					return 1;
+				} else if (failureMap.get(o1) > failureMap.get(o2)) {
+					return -1;
+				}
+				return 0;
+			}
+			
+		});
+		
+		int[] answer = new int[keySetList.size()];
+		for(int i = 0 ; i < keySetList.size() ; i++) {
+			answer[i] = keySetList.get(i);
 		}
-		System.out.println(sum);
 	}
 }
